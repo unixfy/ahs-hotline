ecd65730e994fbae8de4ef758f67b379ad1cc89c
unixfy <admin@unixfy.me>
2021-12-03 14:30:15 -0500

    Bootstrap basic authentication box

---
+++ frontend/src/components/Auth/AuthBox.svelte
@@ -0,0 +1,31 @@
+<script>
+    import {PasswordInput, Button, Form, FormGroup, Tile} from "carbon-components-svelte";
+
+    // User's input
+    let authTicket = "";
+    // Sets whether the input field is shown invalid
+    let ticketValidationFailed = false;
+    let ticketValidationFailureText = '';
+
+    let validateTicket = function () {
+        ticketValidationFailed = true;
+        ticketValidationFailureText = 'This code does not exist.';
+    };
+
+</script>
+
+<Tile>
+    <h2>Log in with an authentication code</h2>
+    <p>You can use this to send a new message or view an old one.</p>
+
+    <hr>
+
+    <Form on:submit={validateTicket}>
+        <FormGroup>
+            <PasswordInput bind:value={authTicket} placeholder="Enter 8-digit code..." invalid={ticketValidationFailed}
+                           labelText="Authentication Code" hidePasswordLabel="Hide code" showPasswordLabel="Show code"
+                           invalidText={ticketValidationFailureText}/>
+        </FormGroup>
+        <Button type="submit">Submit</Button>
+    </Form>
+</Tile>
