4056eef72ae8d3a69f279344794451b54423042c
unixfy <admin@unixfy.me>
2021-12-03 19:07:13 -0500

    Enable hiding stuff from navbar with routify hidden metadata

--- frontend/src/components/Navbar/NavItems.svelte
+++ frontend/src/components/Navbar/NavItems.svelte
@@ -23,30 +23,35 @@
 </script>
 
 <HeaderNav>
-    {#each items as {path, title, children, ...rest}}
-        {#if children.length > 0}
-            <HeaderNavMenu text="{toTitlecase(title)}">
-                {#each children as {path, title, ...rest}}
-                    <HeaderNavItem href="{$url(path)}" isSelected={$isActive(path)} text="{toTitlecase(title)}"/>
-                {/each}
-            </HeaderNavMenu>
-        {:else}
-            <HeaderNavItem href="{$url(path)}" isSelected={$isActive(path)} text="{toTitlecase(title)}"/>
+    {#each items as {path, title, children, hidden, ...rest}}
+        {#if !(hidden = true)}
+            {#if children.length > 0}
+                <HeaderNavMenu text="{toTitlecase(title)}">
+                    {#each children as {path, title, ...rest}}
+                        <HeaderNavItem href="{$url(path)}" isSelected={$isActive(path)} text="{toTitlecase(title)}"/>
+                    {/each}
+                </HeaderNavMenu>
+            {:else}
+                <HeaderNavItem href="{$url(path)}" isSelected={$isActive(path)} text="{toTitlecase(title)}"/>
+            {/if}
         {/if}
     {/each}
 </HeaderNav>
 
 <SideNav bind:isOpen={sideNavOpen}>
     <SideNavItems>
-        {#each items as {path, title, children, ...rest}}
-            {#if children.length > 0}
-                <SideNavMenu text="{toTitlecase(title)}">
-                    {#each children as {path, title, ...rest}}
-                        <SideNavMenuItem href="{$url(path)}" isSelected={$isActive(path)}  text="{toTitlecase(title)}"/>
-                    {/each}
-                </SideNavMenu>
-            {:else}
-                <SideNavLink href="{$url(path)}" isSelected={$isActive(path)} text="{toTitlecase(title)}"/>
+        {#each items as {path, title, children, hidden, ...rest}}
+            {#if !(hidden = true)}
+                {#if children.length > 0}
+                    <SideNavMenu text="{toTitlecase(title)}">
+                        {#each children as {path, title, ...rest}}
+                            <SideNavMenuItem href="{$url(path)}" isSelected={$isActive(path)}
+                                             text="{toTitlecase(title)}"/>
+                        {/each}
+                    </SideNavMenu>
+                {:else}
+                    <SideNavLink href="{$url(path)}" isSelected={$isActive(path)} text="{toTitlecase(title)}"/>
+                {/if}
             {/if}
         {/each}
     </SideNavItems>
