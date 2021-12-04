9a5cee914d125b2ca6209a0381080b1ebf2ba599
unixfy <admin@unixfy.me>
2021-12-04 15:38:54 -0500

    Refactor messages URL a bit

--- frontend/src/pages/admin/_layout.svelte
+++ frontend/src/pages/admin/_layout.svelte
@@ -1,30 +1,36 @@
 <script>
     import {url} from '@roxi/routify'
-    import {ClickableTile, Column, Row} from "carbon-components-svelte";
+    import {ClickableTile, Column, InlineNotification, Row} from "carbon-components-svelte";
 </script>
 
-<Row>
-    <Column md={4} sm={4} lg={4}>
-        <h2 style="padding-bottom: 30px;">Admin Area</h2>
-        <ClickableTile href="{$url('/admin/auth-tickets')}">
-            Authentication Tickets
-        </ClickableTile>
-        <ClickableTile href="{$url('/admin/messages')}">
-            All Messages
-        </ClickableTile>
-        <ClickableTile href="{$url('/admin/my-messages')}">
-            My Messages
-        </ClickableTile>
-        <ClickableTile href="{$url('/admin/taxonomies')}">
-            Taxonomies
-        </ClickableTile>
-        <ClickableTile href="{$url('/admin/logout')}">
-            Logout
-        </ClickableTile>
-    </Column>
-    <Column md={12} sm={12} lg={12}>
-        <slot></slot>
-    </Column>
-</Row>
+{#if false}
+    <Row>
+        <Column md={4} sm={4} lg={4}>
+            <h2 style="padding-bottom: 30px;">Admin Area</h2>
+            <ClickableTile href="{$url('/admin/auth-tickets')}">
+                Authentication Tickets
+            </ClickableTile>
+            <ClickableTile href="{$url('/admin/message/all')}">
+                All Messages
+            </ClickableTile>
+            <ClickableTile href="{$url('/admin/message/my')}">
+                My Messages
+            </ClickableTile>
+            <ClickableTile href="{$url('/admin/taxonomies')}">
+                Taxonomies
+            </ClickableTile>
+            <ClickableTile href="{$url('/admin/logout')}">
+                Logout
+            </ClickableTile>
+        </Column>
+        <Column md={12} sm={12} lg={12}>
+            <slot></slot>
+        </Column>
+    </Row>
+{:else}
+    <h1>Oops!</h1>
+    <InlineNotification title="Unauthorized" subtitle="You aren't allowed to access this page!" hideCloseButton
+                        kind="error"/>
+{/if}
 <!-- routify:options hidden -->
 
