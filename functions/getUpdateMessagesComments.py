7e79f0025705f98e1736b59900130c792cb0d269
unixfy <admin@unixfy.me>
2021-12-04 15:39:02 -0500

    Bootstrap functions

---
+++ functions/getUpdateMessagesComments.py
@@ -0,0 +1,5 @@
+# TODO: Take input - auth code, and tenant ID
+# If a ticket exists, return a json object with ticket object and all responses
+# If a ticket does not exist, prompt the user to create a ticket
+# Also take & sanitize input to create a ticket (i.e. subject, content, attachment(s), category, priority, tenant)
+# Also take & sanitize input to create a response (i.e. content, attachment(s), ticket to be linked to)
---
+++ functions/validations.py
@@ -0,0 +1,3 @@
+# TODO: Handle validations: Ensure color field of all priorities, categories, statuses are hexadecimal colors
+# Ensure that a user profile is always created when a user is created
+# Ensure that a user profile is always deleted when a user is deleted
