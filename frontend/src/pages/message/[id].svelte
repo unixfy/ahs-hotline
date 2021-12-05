78e3936fb8081c6c3873b5973ca722630d3d3ec1
unixfy <admin@unixfy.me>
2021-12-04 19:19:39 -0500

    Make the logout button more dangerous

--- frontend/src/pages/message/[id].svelte
+++ frontend/src/pages/message/[id].svelte
@@ -1,6 +1,12 @@
 <script>
     import PageHeader from "../../components/PageHeader/PageHeader.svelte";
+    import MessageDisplay from "../../components/Message/MessageDisplay.svelte";
 
     export let id;
 </script>
+
 <PageHeader title="View message: {id}" subtitle="Last updated: TIME by USER"/>
+
+<MessageDisplay id={id} />
+
+<!-- routify:options title="View Message" -->
