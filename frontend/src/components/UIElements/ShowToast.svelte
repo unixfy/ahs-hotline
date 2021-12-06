2ab94e68e9717596e8f908618dfcf96b9d9510e2
unixfy <admin@unixfy.me>
2021-12-06 18:57:18 -0500

    Add a cool animation (built into svelte) to toasts :)

--- frontend/src/components/UIElements/ShowToast.svelte
+++ frontend/src/components/UIElements/ShowToast.svelte
@@ -1,13 +1,18 @@
 <script>
     import {ToastNotification} from "carbon-components-svelte";
     import {toast} from "../../stores";
+    import {scale} from "svelte/transition"
 </script>
 
 {#each $toast.queue as toast}
-    <ToastNotification
-            kind={toast.type}
-            title={toast.title}
-            subtitle={toast.message}
-            caption={new Date().toLocaleString()}
-    />
+    <div transition:scale>
+        <ToastNotification
+                kind={toast.type}
+                title={toast.title}
+                subtitle={toast.message}
+                caption={new Date().toLocaleString()}
+                hideCloseButton
+        />
+    </div>
+
 {/each}
